41c304546efbbdd0f35c2642c3ecd19722dcf819
Yuxin Wu <ppwwyyxxc@gmail.com>
2021-02-01 16:09:36 -0800

    fix bug in rleToBbox

--- tests/test_cases.py
+++ tests/test_cases.py
@@ -13,6 +13,7 @@ class TestToBBox(unittest.TestCase):
             (bbox == np.array([0, 0, 2, 2], dtype="float32")).all(),
             bbox)
 
+    # bugfix by piotr in ff4a47150bf66
     def testToBboxNonFullImage(self):
         mask = np.zeros((10, 10), dtype=np.uint8)
         mask[2:4, 3:6] = 1
@@ -20,6 +21,18 @@ class TestToBBox(unittest.TestCase):
         self.assertTrue(
             (bbox == np.array([3, 2, 3, 2], dtype="float32")).all(),
             bbox)
+
+    def testZeroLeadingRLE(self):
+        # A foreground segment of length 0 was not previously handled correctly.
+        # This input rle has 3 leading zeros.
+        rle = {'size': [1350, 1080], 'counts': '000lg0Zb01O00001O00001O001O00001O00001O001O00001O01O2N3M3M3M2N3M3N2M3M2N1O1O1O1O2N1O1O1O2N1O1O101N1O1O1O2N1O1O1O2N3M2N1O2N1O2O0O2N1O1O2N1O2N1O2N1O2N1O2N1O2O0O2N1O3M2N1O2N2N2N2N2N1O2N2N2N2N1O2N2N2N2N2N1N3N2N00O1O1O1O100000000000000O100000000000000001O0000001O00001O0O5L7I5K4L4L3M2N2N2N1O2m]OoXOm`0Sg0j^OVYOTa0lf0c^O]YO[a0ef0\\^OdYOba0bg0N2N2N2N2N2N2N2N2N2N2N2N2N2N2N2N2N3M2M4M2N3M2N3M2N3M2N3M2N3M2N3M2N3M2N3M2M4M2N2N3M2M4M2N2N3M2M3N3M2N3M2M3N3M2N2N3L3N2N3M2N3L3N2N3M5J4M3M4L3M3L5L3M3M4L3L4\\EXTOd6jo0K6J5K6I4M1O1O1O1N2O1O1O001N2O00001O0O101O000O2O00001N101N101N2N101N101N101N2O0O2O0O2O0O2O1N101N2N2O1N2O1N2O1N101N2O1N2O1N2O0O2O1N2N2O1N2O0O2O1N2O1N2N2N1N4M2N2M4M2N3L3N2N3L3N3L3N2N3L3N2N3L3M4L3M3M4L3M5K5K5K6J5K5K6J7I7I7Ibijn0'}
+        orig_bbox = mask_util.toBbox(rle)
+        mask = mask_util.decode(rle)
+        rle_new = mask_util.encode(mask)
+        new_bbox = mask_util.toBbox(rle_new)
+        print(new_bbox)
+        assert np.equal(orig_bbox, new_bbox).all()
+
 
 if __name__ == "__main__":
     unittest.main()
