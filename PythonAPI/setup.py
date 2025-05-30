d1fe2a33ff48a43bb7c935d568586a746098352a
Dowon <ks2515@naver.com>
2025-05-30 17:23:19 +0900

    fix: remove abi3 features

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -1,33 +1,17 @@
 """To compile and install locally run "python setup.py build_ext --inplace".
 To install library to Python site-packages run "python -m pip install --use-feature=in-tree-build ."
 """
-import platform
-import sys
-import sysconfig
 from pathlib import Path
 from setuptools import setup, Extension
 
 import numpy as np
 from Cython.Build import cythonize
 
-py_gil_disabled = sysconfig.get_config_var('Py_GIL_DISABLED')
-use_limited_api = not py_gil_disabled and platform.python_implementation() == 'CPython' and sys.version_info >= (3, 12)
-if use_limited_api:
-    limited_api_args = {
-        "py_limited_api": True,
-        "define_macros": [("Py_LIMITED_API", "0x030C0000")],
-    }
-    options = {"bdist_wheel": {"py_limited_api": "cp312"}}
-else:
-    limited_api_args = {}
-    options = {}
-
 ext_modules = [
         Extension(
             'pycocotools._mask',
             sources=['./common/maskApi.c', 'pycocotools/_mask.pyx'],
             include_dirs=[np.get_include(), './common'],
-            **limited_api_args
         )
     ]
 
@@ -54,5 +38,4 @@ setup(
     },
     version='2.0.9',
     ext_modules=cythonize(ext_modules),
-    options=options,
 )
