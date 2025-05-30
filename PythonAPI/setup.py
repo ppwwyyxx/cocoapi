fd8253ef517a5beb5cdf4049e48872e9a51a1fd7
Dowon <ks2515@naver.com>
2025-05-30 20:06:36 +0900

    ~~Build abi3 wheel for python 3.13, 3.14...~~ and build modernizing (#27)
    * limited api test
    
    * update build
    
    * cython?
    
    * no compile args
    
    * fix abi3audit command
    
    * pybuffer 3.11
    
    * fix abi3audit
    
    * fix sysconfig Py_GIL_DISABLED
    
    * abi3 - python3.12
    
    https://cython.readthedocs.io/en/latest/src/userguide/limited_api.html#performance
    
    * Constrains numpy version and updates setuptools
    
    https://github.com/scipy/oldest-supported-numpy#deprecation-notice-for-numpy-20
    https://setuptools.pypa.io/en/latest/history.html#v70-1-0
    
    * update unittest
    
    * fix: remove abi3 features
    
    * fix: do not include _mask.c file in the sdist

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -1,19 +1,17 @@
 """To compile and install locally run "python setup.py build_ext --inplace".
 To install library to Python site-packages run "python -m pip install --use-feature=in-tree-build ."
 """
-import platform
 from pathlib import Path
 from setuptools import setup, Extension
 
 import numpy as np
+from Cython.Build import cythonize
 
 ext_modules = [
         Extension(
             'pycocotools._mask',
             sources=['./common/maskApi.c', 'pycocotools/_mask.pyx'],
             include_dirs=[np.get_include(), './common'],
-            extra_compile_args=[] if platform.system()=='Windows' else
-            ['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
         )
     ]
 
@@ -21,7 +19,6 @@ try:
     readme = Path(__file__).parent.parent.joinpath("README.md").read_text("utf-8")
 except FileNotFoundError:
     readme = ""
-
 
 setup(
     name='pycocotools',
@@ -40,5 +37,5 @@ setup(
         'all': ['matplotlib>=2.1.0'],
     },
     version='2.0.9',
-    ext_modules=ext_modules
+    ext_modules=cythonize(ext_modules),
 )
